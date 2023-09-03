8ef91261aa6c40234ee892c974a882e990591663
krunalghaskata <krunalghaskata1223@gmail.com>
2023-09-03 22:31:03 +0530

    file commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// console.log('hello world');
+require("dotenv").config();
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+
+const PORT = process.env.PORT;
+
+const filePathByAsync = fs.readFile("index.html", "utf-8", (error, data) => {
+  // console.log('filePathByAsync', data);
+});
+const filePathBySync = fs.readFileSync("index.html", "utf-8");
+// console.log('filePathBySync', filePathBySync);
+
+const filePath = path.join(__dirname, "index.html");
+// module wrapper function
+// https://www.geeksforgeeks.org/how-to-change-the-node-js-module-wrapper/
+// console.log(filePath);
+// console.log(__dirname);
+// console.log(__filename);
+// console.log('exports', exports);
+// console.log('require', require);
+// console.log('module', module);
+
+// const server = http.createServer((req, res) => {
+//   if (req.url == '/') {
+//     res.end('hello nodejs')
+//   } else if (req.url == '/login') {
+//     res.end('<h1>You are in login page</h1>')
+//   } else if (req.url == '/html') {
+//     res.setHeader('Content-type', 'text/html')
+//     res.end(filePathBySync);
+//   } else {
+//     res.end('<h1>404 not found</h1>')
+//   }
+// })
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("hello nodejs");
+});
+server.get("/login", (req, res) => {
+  res.send("<h1>You are in login page</h1>");
+});
+server.get("/html", (req, res) => {
+  res.sendFile(filePath);
+});
+server.get("*", (req, res) => {
+  res.send("<h1>404 not found</h1>");
+});
+
+server.listen(PORT, () => {
+  console.log(`server is started at http://localhost:${PORT}`);
+});
